6d9f1730cfda1d6f15486dc630c778f1cbb52371
Dina <dcaraballo26@gmail.com>
2019-02-13 21:42:54 -0700

    Begin setup of graphql endpoint

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require('express');
-const bodyParser = requre('body-parser');
+const bodyParser = require('body-parser');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use('/api', graphqlHTTP({
+  schema: buildSchema(`
+    type RootQuery {
+      
+    }
+
+    type RootMutation {
+
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {}
+}));
 
 app.listen(4000);
