ced040a6d8266f295f40edbc230441647615a75c
Dina <dcaraballo26@gmail.com>
2019-02-13 22:33:05 -0700

    Add to schema

--- app.js
+++ app.js
@@ -5,16 +5,37 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 
 app.use('/api', graphqlHTTP({
   schema: buildSchema(`
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      date: String!
+      contact: String!
+      location: String!
+      price_quote: Float!
+    }
+
+    input EventInput {
+      title: String!
+      description: String!
+      date: String!
+      contact: String!
+      location: String!
+      price_quote: Float!
+    }
+
     type RootQuery {
-      events: [String!]!
+      events: [Event!]!
     }
 
     type RootMutation {
-      createEvent(name: String): String
+      createEvent(eventInput: EventInput): Event
     }
 
     schema {
@@ -24,11 +45,20 @@ app.use('/api', graphqlHTTP({
   `),
   rootValue: {
     events: () => {
-      return ['Smith Wedding', 'Isakov Bar Mitzvah', 'Rodriguez Quinceanera'];
+      return events;
     },
     createEvent: (args) => {
-      const eventName = args.name;
-      return eventName;
+      const event = {
+        _id: Math.random().toString(),
+        title: args.eventInput.title,
+        description: args.eventInput.description,
+        date: new Date().toISOString(),
+        contact: args.eventInput.contact,
+        location: args.eventInput.location,
+        price_quote: +args.eventInput.price_quote
+      };
+      events.push(event);
+      return event;
     }
   },
   graphiql: true
