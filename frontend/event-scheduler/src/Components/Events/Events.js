4d6e52e0a5d8995df014814b7c85332c58c0a9e0
Dina <dcaraballo26@gmail.com>
2019-02-16 11:39:57 -0700

    Add modal form and styling

--- frontend/event-scheduler/src/Components/Events/Events.js
+++ frontend/event-scheduler/src/Components/Events/Events.js
@@ -41,10 +41,35 @@ class Events extends Component {
             userConfirm
             onCancel={this.handleCancelClick}
             onConfirm={this.handleConfirmClick}>
-          <p>Modal Content</p>
+            <form className="events-form">
+              <div className="events-form-controls">
+                <label htmlFor="event-title">Title</label>
+                <input type="text" class="event-title"></input>
+              </div>
+              <div className="events-form-controls">
+                <label htmlFor="event-date">Date</label>
+                <input type="date" class="event-date"></input>
+              </div>
+              <div className="events-form-controls">
+                <label htmlFor="event-contact">Contact</label>
+                <input type="text" class="event-contact"></input>
+              </div>
+              <div className="events-form-controls">
+                <label htmlFor="event-location">Location</label>
+                <input type="text" class="event-location"></input>
+              </div>
+              <div className="events-form-controls">
+                <label htmlFor="event-price">Price</label>
+                <input type="number" class="event-price"></input>
+              </div>
+              <div className="events-form-controls">
+                <label htmlFor="event-description">Price</label>
+                <textarea class="event-description" rows="4"></textarea>
+              </div>
+            </form>
           </Modal>
         )}
-        <div className="events-control">
+        <div className="events-controls">
           <p>Share your own events!</p>
           <button
             className="event-btn"
