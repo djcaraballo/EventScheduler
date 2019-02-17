e98abe8c85a7e96cca41e47736702527de999be2
Dina <dcaraballo26@gmail.com>
2019-02-17 09:28:53 -0700

    Add functionality to retrieve and display bookings

--- frontend/event-scheduler/src/Components/Bookings/Bookings.js
+++ frontend/event-scheduler/src/Components/Bookings/Bookings.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react';
 
+import AuthContext from '../../context/authContext';
+import LoadingGif from '../Loading/Loading';
+import { fetchData } from '../../API/api';
+
 class Bookings extends Component {
+  static contextType = AuthContext
+
   constructor(props) {
     super(props)
     this.state = {
-
+      isLoading: false,
+      bookings: [],
     }
+  }
+
+  componentDidMount() {
+    this.getBookings();
+  };
+
+  getBookings = async () => {
+    this.setState({isLoading: true})
+    const requestBody = {
+      query: `
+        query {
+          bookings {
+            _id
+            createdAt
+            event {
+              _id
+              title
+              date
+            }
+          }
+        }
+      `
+    }
+    const token = this.context.token
+    const bookings = await fetchData(requestBody, token);
+    this.setState({
+      bookings: bookings.data.bookings
+    })
+    this.setState({isLoading: false});
   }
 
   render() {
     return(
-      <div>
-        <h1>The Bookings Page</h1>
-      </div>
+      <React.Fragment>
+      {this.state.isLoading ? (
+        <LoadingGif />
+      ) : (
+        <ul>
+        {this.state.bookings.map(booking => <li key={booking._id}>{booking.event.title} -- {new Date(booking.createdAt).toLocaleDateString()}</li>)}
+        </ul>
+      )}
+      </React.Fragment>
     )
   }
 }
