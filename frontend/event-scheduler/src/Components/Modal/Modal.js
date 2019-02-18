f2426f75148bab606e79ffa642d0d9a8f1409c9e
Dina <dcaraballo26@gmail.com>
2019-02-18 01:49:22 -0700

    Add image to modal

--- frontend/event-scheduler/src/Components/Modal/Modal.js
+++ frontend/event-scheduler/src/Components/Modal/Modal.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './Modal.css'
+const mixer = require('../../assets/mixer.jpg')
+const mixer2 = require('../../assets/mixer2.jpg')
+const networking = require('../../assets/networking.jpg')
+const networking2 = require('../../assets/networking2.jpg')
 
 const Modal = props => {
+  const imgUrls = [
+    mixer,
+    mixer2,
+    networking,
+    networking2
+  ]
+  const srcUrl = imgUrls[Math.floor(Math.random() * Math.floor(4))]
+  console.log(srcUrl)
+
   return (
     <div className="modal">
       <header className="modal-header">
         <h1>{props.title}</h1>
+        <img src={srcUrl}  />
       </header>
       <section className="modal-content">
         {props.children}
